f191ba8f1e458e2698b1c79568f0f2c8e18bf3f6
guilherme.santos <guilherme.santos@seveninovacoes.com.br>
2026-04-22 02:20:40 -0300

    implementacao controle de estoque

--- ExemploGitHub/Program.cs
+++ ExemploGitHub/Program.cs
@@ -3,3 +3,5 @@ Console.ReadLine();
 
 //Git merge: mescla alterações de diferentes branches em um unico branch
 //Pull request: soilicitação para revisar e mesclar.
+
+//Git Flow: fluxo de trabalho que define com os desenvolvedores coloaboram usando branches e commits no git.
